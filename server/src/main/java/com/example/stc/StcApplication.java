514fb2e2bafc6754083750458710c31aa473624c
lfm <1071956678@qq.com>
2019-05-19 18:44:36 +0800

    完成activiti依赖

--- server/src/main/java/com/example/stc/StcApplication.java
+++ server/src/main/java/com/example/stc/StcApplication.java
@@ -1,11 +1,12 @@
 package com.example.stc;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class StcApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
