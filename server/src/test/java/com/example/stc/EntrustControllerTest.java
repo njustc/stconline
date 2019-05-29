f0ed54735c5edbaf0a131f0817d5478adccb9e49
lfm <1071956678@qq.com>
2019-05-29 17:38:45 +0800

    test

--- server/src/test/java/com/example/stc/EntrustControllerTest.java
+++ server/src/test/java/com/example/stc/EntrustControllerTest.java
@@ -1,34 +1,39 @@
 package com.example.stc;
 
+import com.alibaba.fastjson.JSON;
 import com.example.stc.controller.CustomerEntrustController;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureWebMvc
 @AutoConfigureMockMvc
-public class EntrustController {
+public class EntrustControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private CustomerEntrustController entrustController;
+
+    private static final Logger logger = LoggerFactory.getLogger(EntrustControllerTest.class);
 
     @Test
     public void notNull() throws Exception {
@@ -43,9 +48,15 @@ public class EntrustController {
     public void httpTest() throws Exception {
         mockMvc.perform(get("/api/customers/projects"))
                 .andExpect(status().isOk());
+        Map<String, Object> reqBody = new HashMap<>();
+        reqBody.put("pid", "hello");
+        logger.info("sent post with body " + JSON.toJSONString(reqBody));
         //新建委托
-//        mockMvc.perform(post("/api/customers/projects")
-//                .contentType("application/json"))
-//                .andExpect(status().isCreated());
+        MvcResult result = mockMvc.perform(post("/api/customers/projects")
+                .content(JSON.toJSONString(reqBody))
+                .contentType("application/json"))
+                .andExpect(status().isCreated())
+                .andReturn();
+
     }
 }
