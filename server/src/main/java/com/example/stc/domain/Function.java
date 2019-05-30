54f05b583b4e624cc706d9f706b63dae8bd67dce
Novel <novelist.chan@gmail.com>
2019-05-30 12:42:00 +0800

    edit User

--- server/src/main/java/com/example/stc/domain/Function.java
+++ server/src/main/java/com/example/stc/domain/Function.java
@@ -4,5 +4,5 @@ package com.example.stc.domain;
 /**
  * Funtion指明Role所对应的具体权限
  */
-public class Funtion {
+public class Function {
 }
--- server/src/main/java/com/example/stc/domain/User.java
+++ server/src/main/java/com/example/stc/domain/User.java
@@ -1,9 +1,10 @@
 package com.example.stc.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Column;
+import com.alibaba.fastjson.annotation.JSONField;
+
+import javax.persistence.*;
+import java.util.List;
 
 /**
  * 用户账号类
@@ -22,4 +23,49 @@ public class User extends NamedEntity {
      */
     @Column(name = "PASSWORD")
     private String password;
+
+
+    /**
+     * 联接表：用户与角色，多对多
+     */
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "TBL_SYS_ROLE_USERS", joinColumns =
+            {
+                    @JoinColumn(name = "USER_ID", foreignKey = @ForeignKey(name = "none", value = ConstraintMode.NO_CONSTRAINT))
+            }, inverseJoinColumns =
+            {
+                    @JoinColumn(name = "ROLE_ID", foreignKey = @ForeignKey(name = "none", value = ConstraintMode.NO_CONSTRAINT))
+            })
+
+    /**
+     * 用户所对应的角色组
+     */
+    private List<Role> roles;
+
+
+    /**
+     * 用户的委托列表
+     */
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID",foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT))
+    @JSONField(serialize = false)
+    private List<Entrust> entrusts;
+
+    /**
+     * 用户的项目列表
+     */
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID",foreignKey = @ForeignKey(name = "none",value = ConstraintMode.NO_CONSTRAINT))
+    @JSONField(serialize = false)
+    private List<Project> projects;
+
+    public List<Project> getProjects(){
+        return projects;
+    }
+
+    /**
+     * 用户权限组
+     */
+    @Transient
+    private List<Function> functions;
 }
