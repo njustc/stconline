64589fea45d10120b43d3ffcf5c27547c619a0af
Novel <novelist.chan@gmail.com>
2019-05-30 12:16:01 +0800

    add Role&Funtion definition

---
+++ server/src/main/java/com/example/stc/domain/Funtion.java
@@ -0,0 +1,8 @@
+package com.example.stc.domain;
+
+
+/**
+ * Funtion指明Role所对应的具体权限
+ */
+public class Funtion {
+}
---
+++ server/src/main/java/com/example/stc/domain/Role.java
@@ -0,0 +1,8 @@
+package com.example.stc.domain;
+
+
+/**
+ * 每个用户User对应一个角色Role，一个Role包含若干个Funtion
+ */
+public class Role {
+}
