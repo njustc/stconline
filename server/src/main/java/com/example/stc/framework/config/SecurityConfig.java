cc95f91e55e48d347ae4cf79865dafb08f18268a
1Jyfc <934582192@qq.com>
2019-06-05 22:07:13 +0800

    联调初步完成，可运行，问题列表见PROBLEM.md

--- server/src/main/java/com/example/stc/framework/config/SecurityConfig.java
+++ server/src/main/java/com/example/stc/framework/config/SecurityConfig.java
@@ -44,12 +44,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 // 使用Spring默认登出URL: http://localhost:8080/logout(http://localhost:8080/login?logout)
 
-                .and()
-                .authorizeRequests() // 定义需要保护和不需要保护的URL
-                .antMatchers("/register", "/api/register/**", "/login", "/api/login").permitAll()
-                .antMatchers("/api/projects/**").hasRole(Role.USER.str())
-                // TODO: 其他针对角色拦截的URL
-                .anyRequest().authenticated() // 任何请求,登录后可以访问
+//                .and()
+//                .authorizeRequests() // 定义需要保护和不需要保护的URL
+//                .antMatchers("/register", "/api/register/**", "/login", "/api/login").permitAll()
+//                .antMatchers("/api/projects/**").hasRole(Role.USER.str())
+//                // TODO: 其他针对角色拦截的URL
+//                .anyRequest().authenticated() // 任何请求,登录后可以访问
 
                 .and().csrf().disable();
     }
