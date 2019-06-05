e85ae1ca763a2b562caac75e6678abbd98862ea3
DCTHB <50065084+DCTHB@users.noreply.github.com>
2019-06-05 15:44:40 +0800

    搭建了合同编辑页面的框架

---
+++ client/src/pages/contract/contract_edit/locales/zh-CN.js
@@ -0,0 +1,7 @@
+export default {
+	'contract.project_name.label':'项目名称',
+	'validation.project_name.required': '请填写项目名称！',
+	'contract.project_name.placeholder':' ',
+	
+	'contract.sign_data.label': '签订日期',
+};
