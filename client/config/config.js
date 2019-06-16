1353b16c170e97fca109fac2dc4584c624e36f5b
lfm <1071956678@qq.com>
2019-06-16 18:00:27 +0800

    实现了正常的打包并且运行; 需要后端自行把认证重定向到登录页面

--- client/config/config.js
+++ client/config/config.js
@@ -3,7 +3,7 @@ import {primaryColor} from '../src/defaultSettings';
 
 export default {
   base:'/',
-  publicPath: 'http://localhost:8000/',
+  publicPath: 'http://localhost:8080/',
   treeShaking: true,
   exportStatic:{
     htmlSuffix:true,
@@ -40,56 +40,64 @@ export default {
   /**
    * 路由相关配置
    */
-  routes: [
-    {
-      path: '/user',
-      component: '../layouts/UserLayout',
-      routes: [
-        {
-          path: '/user',
-          component: './Welcome',
-        },
-      ],
-    },
-    {
-      path: '/',
-      component: '../layouts/BasicLayout',
-      routes: [
-        {
-          path: '/',
-          redirect: '/welcome',
-        },
-        // dashboard
-        {
-          path: '/welcome',
-          name: 'welcome',
-          icon: 'smile',
-          component: './Welcome',
-        },
-        // {
-        //   path: 'https://github.com/umijs/umi-blocks/tree/master/ant-design-pro',
-        //   name: 'more-blocks',
-        //   icon: 'block',
-        // },
-        {
-          //name: 'basic-form',
-          //icon: 'dashboard',
-          path: '/basic-form',
-          component: './entrust/basic-form',
-        },
-        //entrustlist
-        {
-          name: '您的委托',
-          icon: 'pie-chart',
-          path: '/basic-list',
-          component: './entrust/basic-list/entrustlist',
-        },
-        {
-          path: '/basic-check',
-          component: './entrust/basic-check',
-        },
-      ],
-    },
+  routes: [{
+    path: '/',
+    component: '../layouts/BasicLayout',
+    routes: [
+      {
+        path: '/',
+        redirect: '/welcome',
+      },
+      // dashboard
+      {
+        path: '/welcome',
+        name: 'welcome',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      // {
+      //   path: 'https://github.com/umijs/umi-blocks/tree/master/ant-design-pro',
+      //   name: 'more-blocks',
+      //   icon: 'block',
+      // },
+      {
+        //name: 'basic-form',
+        //icon: 'dashboard',
+        path: '/basic-form',
+        component: './entrust/basic-form',
+      },
+      //entrustlist
+      {
+        name: '您的委托',
+        icon: 'pie-chart',
+        path: '/basic-list',
+        component: './entrust/basic-list/entrustlist',
+      },
+      {
+        //name: '委托详情',
+        path: '/basic-check',
+        component: './entrust/basic-check',
+      },
+      {
+        name: '您的合同详情',
+        icon: 'pie-chart',
+        path: '/contract_detail',
+        component: './contract/contract_detail',
+      },
+      {
+        name: '您的合同列表',
+        icon: 'pie-chart',
+        path: '/contract_list',
+        component: './contract/contract_list',
+      },
+      {
+        name: '您的合同编辑',
+        icon: 'pie-chart',
+        path: '/contract_edit',
+        component: './contract/contract_edit',
+      },
+    ],
+  },
   ],
   disableRedirectHoist: true,
   /**
@@ -101,11 +109,5 @@ export default {
   theme: {'primary-color': primaryColor},
   externals: {'@antv/data-set': 'DataSet'},
   ignoreMomentLocale: true,
-  lessLoaderOptions: {javascriptEnabled: true},
-  proxy: {
-    '/api': {
-      target: 'http://localhost:8080',
-      changeOrigin: true,
-    }
-  }
+  lessLoaderOptions: {javascriptEnabled: true}
 };
