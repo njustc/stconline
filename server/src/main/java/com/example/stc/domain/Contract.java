e3d7e95e79adf9eabfbb6269ffc3dc1cc0e1f31c
AldebaRain <2904428882@qq.com>
2019-06-23 16:48:51 +0800

    Update Contract.java

--- server/src/main/java/com/example/stc/domain/Contract.java
+++ server/src/main/java/com/example/stc/domain/Contract.java
@@ -28,10 +28,10 @@ public class Contract extends ProcessEntity {
     private String signPlace;
 
     /**
-     *签订日期
+     * 签订日期
      */
-    @Column(name = "SIGNDATA")
-    private String signData;
+    @Column(name = "SIGNDATE")
+    private String signDate;
 
     /**
      * 质量特性
@@ -66,8 +66,8 @@ public class Contract extends ProcessEntity {
     /**
      * 委托方--签章日期
      */
-    @Column (name = "CLIENTSIGNDATA")
-    private String clientSignData;
+    @Column (name = "CLIENTSIGNDATE")
+    private String clientSignDate;
 
     /**
      * 委托方--联系人
@@ -127,8 +127,8 @@ public class Contract extends ProcessEntity {
     /**
      * 受托方--签章日期
      */
-    @Column (name = "ASSIGNEESIGNDATA")
-    private String assigneeSignData;
+    @Column (name = "ASSIGNEESIGNDATE")
+    private String assigneeSignDate;
 
     /**
      * 受托方--联系人
@@ -159,13 +159,13 @@ public class Contract extends ProcessEntity {
      * 受托方--开户银行
      */
     @Column (name = "ASSIGNEEBANK")
-    private String assigneeDataBank;
+    private String assigneeBank;
 
     /**
      * 受托方--户名
      */
     @Column (name = "ASSIGNEEBANKNAME")
-    private String assigneeDataBankName;
+    private String assigneeBankName;
 
     /**
      * 受托方--账号
@@ -193,4 +193,243 @@ public class Contract extends ProcessEntity {
     //@JSONField(serialize = false)
     private User user;
 
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public String getSignPlace() {
+        return signPlace;
+    }
+
+    public void setSignPlace(String signPlace) {
+        this.signPlace = signPlace;
+    }
+
+    public String getSignDate() {
+        return signDate;
+    }
+
+    public void setSignDate(String signDate) {
+        this.signDate = signDate;
+    }
+
+    public String getQuality() {
+        return quality;
+    }
+
+    public void setQuality(String quality) {
+        this.quality = quality;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(String finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public String getClientCompanyName() {
+        return clientCompanyName;
+    }
+
+    public void setClientCompanyName(String clientCompanyName) {
+        this.clientCompanyName = clientCompanyName;
+    }
+
+    public String getClientAuthRepre() {
+        return clientAuthRepre;
+    }
+
+    public void setClientAuthRepre(String clientAuthRepre) {
+        this.clientAuthRepre = clientAuthRepre;
+    }
+
+    public String getClientSignDate() {
+        return clientSignDate;
+    }
+
+    public void setClientSignDate(String clientSignDate) {
+        this.clientSignDate = clientSignDate;
+    }
+
+    public String getClientContact() {
+        return clientContact;
+    }
+
+    public void setClientContact(String clientContact) {
+        this.clientContact = clientContact;
+    }
+
+    public String getClientAddr() {
+        return clientAddr;
+    }
+
+    public void setClientAddr(String clientAddr) {
+        this.clientAddr = clientAddr;
+    }
+
+    public String getClientTel() {
+        return clientTel;
+    }
+
+    public void setClientTel(String clientTel) {
+        this.clientTel = clientTel;
+    }
+
+    public String getClientFax() {
+        return clientFax;
+    }
+
+    public void setClientFax(String clientFax) {
+        this.clientFax = clientFax;
+    }
+
+    public String getClientBank() {
+        return clientBank;
+    }
+
+    public void setClientBank(String clientBank) {
+        this.clientBank = clientBank;
+    }
+
+    public String getClientUserName() {
+        return clientUserName;
+    }
+
+    public void setClientUserName(String clientUserName) {
+        this.clientUserName = clientUserName;
+    }
+
+    public String getClientPostCode() {
+        return clientPostCode;
+    }
+
+    public void setClientPostCode(String clientPostCode) {
+        this.clientPostCode = clientPostCode;
+    }
+
+    public String getAssigneeCompanyName() {
+        return assigneeCompanyName;
+    }
+
+    public void setAssigneeCompanyName(String assigneeCompanyName) {
+        this.assigneeCompanyName = assigneeCompanyName;
+    }
+
+    public String getAssigneeAuthRepre() {
+        return assigneeAuthRepre;
+    }
+
+    public void setAssigneeAuthRepre(String assigneeAuthRepre) {
+        this.assigneeAuthRepre = assigneeAuthRepre;
+    }
+
+    public String getAssigneeSignDate() {
+        return assigneeSignDate;
+    }
+
+    public void setAssigneeSignDate(String assigneeSignDate) {
+        this.assigneeSignDate = assigneeSignDate;
+    }
+
+    public String getAssigneeContact() {
+        return assigneeContact;
+    }
+
+    public void setAssigneeContact(String assigneeContact) {
+        this.assigneeContact = assigneeContact;
+    }
+
+    public String getAssigneeAddr() {
+        return assigneeAddr;
+    }
+
+    public void setAssigneeAddr(String assigneeAddr) {
+        this.assigneeAddr = assigneeAddr;
+    }
+
+    public String getAssigneeTel() {
+        return assigneeTel;
+    }
+
+    public void setAssigneeTel(String assigneeTel) {
+        this.assigneeTel = assigneeTel;
+    }
+
+    public String getAssigneeFax() {
+        return assigneeFax;
+    }
+
+    public void setAssigneeFax(String assigneeFax) {
+        this.assigneeFax = assigneeFax;
+    }
+
+    public String getAssigneeBank() {
+        return assigneeBank;
+    }
+
+    public void setAssigneeBank(String assigneeBank) {
+        this.assigneeBank = assigneeBank;
+    }
+
+    public String getAssigneeBankName() {
+        return assigneeBankName;
+    }
+
+    public void setAssigneeBankName(String assigneeBankName) {
+        this.assigneeBankName = assigneeBankName;
+    }
+
+    public String getAssigneeUserName() {
+        return assigneeUserName;
+    }
+
+    public void setAssigneeUserName(String assigneeUserName) {
+        this.assigneeUserName = assigneeUserName;
+    }
+
+    public String getAssigneePostCode() {
+        return assigneePostCode;
+    }
+
+    public void setAssigneePostCode(String assigneePostCode) {
+        this.assigneePostCode = assigneePostCode;
+    }
+
+    public String getContractBody() {
+        return contractBody;
+    }
+
+    public void setContractBody(String contractBody) {
+        this.contractBody = contractBody;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
