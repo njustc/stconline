2dcfe923aff5e50fceff8529bec5af7135e29e3e
DCTHB <50065084+DCTHB@users.noreply.github.com>
2019-07-01 17:24:54 +0800

    deleteCon

--- client/src/pages/contract/contract_list/index.js
+++ client/src/pages/contract/contract_list/index.js
@@ -1,83 +1,7 @@
-import { Table, Divider, Tag ,Breadcrumb, Button} from 'antd';
+import { Table, Divider, Tag ,Breadcrumb, Button, Modal} from 'antd';
 import Link from 'umi/link'
 import React from "react";
 import {connect} from "dva";
-
-
-const columns = [
-  {
-    title: '方案ID',
-    dataIndex: 'pid',
-    key: 'pid',
-    render: text => <a href="javascript:;">{text}</a>,
-  },
-  {
-    title: '受托方',
-    dataIndex: 'assignee',
-    key: 'assignee',
-  },
-  {
-    title: '状态',
-    key: 'processState',
-    dataIndex: 'processState',
-    render: processState => {
-      var color = processState === 'Approve' ? 'purple' : 'green';
-      if (processState === 'ToReview') {
-        color = 'orange';
-      }
-      if (processState === 'ToSubmit') {
-        color = 'blue';
-      }
-      return (
-        <Tag color={color} key={processState}>
-          {processState}
-        </Tag>
-      );
-    }
-  },
-  {
-    title: '操作',
-    key: 'action',
-    render: (key) => (
-      <span>
-        {key.processState == 'Submit' ? <Link to={{pathname: './contract_detail', query: {pid: key.pid}}}>查看详情</Link> :
-          <Link to={{pathname: './contract_detail', query: {pid: key.pid}}}>查看详情</Link>}
-        <Divider type="vertical"/>
-        {/*<a href="/plan_edit.html">编辑</a>*/}
-        {<Link to={{pathname: '../../contract_edit', query: {pid: key.pid}}}>编辑</Link>}
-        <Divider type="vertical"/>
-          <span style={{color: 'red', cursor: 'pointer'}}>删除</span>
-      </span>
-    ),
-  },
-];
-
-// const data = [
-//   {
-//     key: '1',
-//     contract_name: '小猪佩奇测试',
-//     client: "林黛玉",
-//     tags: ['待提交'],
-//   },
-//   {
-//     key: '2',
-//     contract_name: '小猪乔治测试',
-//     client: 123,
-//     tags: ['待审核'],
-//   },
-//   {
-//     key: '3',
-//     contract_name: '小羊苏西测试',
-//     client: '李逵',
-//     tags: ['待确认'],
-//   },
-//   {
-//     key: '4',
-//     contract_name: '飞天小女警测试',
-//     client: '吕布',
-//     tags: ['已确认'],
-//   },
-// ];
 
 const namespace='contractList';
 const mapStateToProps = (state) => {
@@ -95,14 +19,89 @@ const mapDispatchToProps = (dispatch) => {
         type: `${namespace}/queryInitContracts`,
       });
     },
+    queryDeleteCon:(params) =>{
+      dispatch({
+        type:`${namespace}/queryDeleteCon`,
+        payload:params
+      })
+    },
   };
 };
+
+const confirm = Modal.confirm;
 
 @connect(mapStateToProps,mapDispatchToProps)
 class List extends React.Component{
   componentDidMount() {
     this.props.onDidMount();
-  }
+  };
+
+  columns = [
+    {
+      title: '方案ID',
+      dataIndex: 'pid',
+      key: 'pid',
+      render: text => <a href="javascript:;">{text}</a>,
+    },
+    {
+      title: '受托方',
+      dataIndex: 'assignee',
+      key: 'assignee',
+    },
+    {
+      title: '状态',
+      key: 'processState',
+      dataIndex: 'processState',
+      render: processState => {
+        var color = processState === 'Approve' ? 'purple' : 'green';
+        if (processState === 'ToReview') {
+          color = 'orange';
+        }
+        if (processState === 'ToSubmit') {
+          color = 'blue';
+        }
+        return (
+          <Tag color={color} key={processState}>
+            {processState}
+          </Tag>
+        );
+      }
+    },
+    {
+      title: '操作',
+      key: 'action',
+      render: (key) => (
+        <span>
+          {key.processState == 'Submit' ? <Link to={{pathname: './contract_detail', query: {pid: key.pid}}}>查看详情</Link> :
+            <Link to={{pathname: './contract_detail', query: {pid: key.pid}}}>查看详情</Link>}
+          <Divider type="vertical"/>
+          {/*<a href="/plan_edit.html">编辑</a>*/}
+          {<Link to={{pathname: '../../contract_edit', query: {pid: key.pid}}}>编辑</Link>}
+          <Divider type="vertical"/>
+            <span style={{color: 'red', cursor: 'pointer'}} onClick={this.showDeleteConfirm.bind(this,key)}>删除</span>
+        </span>
+      ),
+    },
+  ];
+
+  showDeleteConfirm(key) {
+    var that = this;
+    confirm({
+      title: '您是否需要删除本合同?',
+      content: `合同ID： ${key.pid}   受托方: ${key.assignee}`,
+      okText: '确认删除',
+      okType: 'danger',
+      concelText: '取消',
+      onOK() {
+        that.props.queryDeleteCon({pid: key.pid});
+        console.log("到这里啦");
+      },
+      conCancel() {
+        console.log('Cancel');
+      },
+    });
+  };
+  
 
   render(){
 		return(
@@ -112,7 +111,7 @@ class List extends React.Component{
 			<Breadcrumb.Item>合同列表</Breadcrumb.Item>
 		</Breadcrumb>
 		<br />
-		<Table columns={columns} dataSource={this.props.dataList} />
+		<Table columns={this.columns} dataSource={this.props.dataList} />
 		<Button a href="contract_edit.html">新建</Button>
 		</div>
 		);
