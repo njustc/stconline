c761dca6006cc6b7efd76a9eb08c0612bbf6df95
jktalent <36266392+jktalent@users.noreply.github.com>
2019-07-03 12:12:27 +0800

    代码结构优化

--- client/src/pages/test-report/report-list/model.js
+++ client/src/pages/test-report/report-list/model.js
@@ -1,30 +1,30 @@
 import { getAlltestReport, deleteTestReport} from '@/services/test-report';
 
 export default {
-  namespace:'testReport',
+  namespace:'test-report',
   state: {
     data: [],
   },
   effects:{
-    *GetAlltestReport(_,{call,put}){
-      const response=yield call(getAlltestReport)
-      console.log(response)
-      console.log('_embedded' in response)
+    *GetAlltestReport(_, {call,put}) {
+      const response = yield call(getAlltestReport)
+      // console.log(response)
+      // console.log('_embedded' in response)
       if(!('_embedded' in response)){
-        console.log("put []")
-        yield put({type:'addListData',payload: response})
+        // console.log("put []")
+        yield put({type:'addListData', payload: response})
       }
       else{
-        yield put({type:'addListData',payload: response._embedded.testReports})
+        yield put({type:'addListData', payload: response._embedded.testReports})
       }
     },
     *DeleteTestReport({payload},{call,put}){
       yield call(deleteTestReport, {pid:payload.pid})
       const list = yield call(getAlltestReport)
-      console.log(list)
-      console.log('_embedded' in list)
+      // console.log(list)
+      // console.log('_embedded' in list)
       if(!('_embedded' in list)){
-        console.log("put []")
+        // console.log("put []")
         yield put({type:'addListData',payload: list})
       }
       else{
@@ -32,10 +32,11 @@ export default {
       }
     },
   },
+
   //响应action并修改state
   reducers:{
-    addListData(state,action){
-      return{
+    addListData(state, action) {
+      return {
         ...state,
         data:action.payload,
       }
