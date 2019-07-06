48b3ba1aefa6a1da580ad03d24560bbe80c44d05
161220004 <2904428882@qq.com>
2019-07-06 16:51:23 +0800

    文件上传位置更改

--- client/src/pages/entrust/basic-form/index.js
+++ client/src/pages/entrust/basic-form/index.js
@@ -40,24 +40,6 @@ const namespace = 'entrustForm';
 const Dragger = Upload.Dragger;
 
 
-const props = {
-  name: 'file',
-  multiple: true,
-  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-  onChange(info) {
-    const status = info.file.status;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-};
-
-
 const mapStateToProps = (state) => {
   const entrustdata = state[namespace];
   return {
@@ -246,7 +228,7 @@ class BasicForm extends PureComponent {
     // 文件上传
     const uploadProps = {
       name: 'file',
-      action: '/api/project/files?pid=pid',
+      action: '/api/project/files?pid=' + this.state.pid,
       multiple: true,
       headers: {
         authorization: 'authorization-text',
@@ -262,6 +244,7 @@ class BasicForm extends PureComponent {
           message.error(`${info.file.name} file upload failed.`);
         }
       },
+
     };
 
     return (
