eab4691e4a9c47a3c4f3a5643513b21a3e7ced1a
jktalent <36266392+jktalent@users.noreply.github.com>
2019-07-06 17:35:20 +0800

    Update zh-CN.js

--- client/src/pages/test-record/record-edit/locales/zh-CN.js
+++ client/src/pages/test-record/record-edit/locales/zh-CN.js
@@ -1,30 +1,30 @@
 export default {
-  'testplan.edit.label':'编辑人员',
-  'validation.edit.required': '请填写编辑人员！',
-  'testplan.edit.placeholder':' ',
+  'testrecord.edit.label':'设计人',
+  'validation.edit.required': '请填写设计人！',
+  'testrecord.edit.placeholder':' ',
 
-  'testplan.check.label':'审核人员',
+  'testrecord.check.label':'审核人员',
   'validation.check.required': '请填写审核人员！',
-  'testplan.check.placeholder':' ',
+  'testrecord.check.placeholder':' ',
 
-  'testplan.approve.label':'批准人员',
+  'testrecord.approve.label':'批准人员',
   'validation.approve.required': '请填写批准人员！',
-  'testplan.approve.placeholder':' ',
+  'testrecord.approve.placeholder':' ',
 
-  'testplan.hardware.label':'测试环境-硬件',
+  'testrecord.hardware.label':'测试环境-硬件',
   'validation.hardware.required': '请填写测试硬件！',
-  'testplan.hardware.placeholder':' ',
+  'testrecord.hardware.placeholder':' ',
 
-  'testplan.software.label':'测试环境-软件',
+  'testrecord.software.label':'测试环境-软件',
   'validation.software.required': '请填写测试软件！',
-  'testplan.software.placeholder':' ',
+  'testrecord.software.placeholder':' ',
 
-  'testplan.staff.label':'测试环境-人员',
+  'testrecord.staff.label':'测试环境-人员',
   'validation.staff.required': '请填写测试人员！',
-  'testplan.staff.placeholder':' ',
+  'testrecord.staff.placeholder':' ',
 
-  'plan_edit.form.submit':'提交',
-  'plan_edit.form.save':'保存',
-  'plan_edit.form.delete':'删除',
+  'record-edit.form.submit':'提交',
+  'record-edit.form.save':'保存',
+  'record-edit.form.delete':'删除',
 
 };
