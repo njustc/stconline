1420887bf7d8c64f0618f5094b6ded2a4dbac907
161220004 <2904428882@qq.com>
2019-07-06 16:24:04 +0800

    文件上传位置转到Entrust列表

--- client/src/pages/user-login/index.js
+++ client/src/pages/user-login/index.js
@@ -80,26 +80,6 @@ class LoginPage extends Component {
     const {userInfo, submitting} = this.props;
     const {status} = userInfo.data;
     const {type, autoLogin} = this.state;
-    const uploadProps = {
-      name: 'file',
-      action: '/api/project/files?pid=pid',
-      multiple: true,
-      headers: {
-        authorization: 'authorization-text',
-      },
-      enctype: "multipart/form-data",
-      onChange(info) {
-        if (info.file.status !== 'uploading') {
-          console.log(info.file, info.fileList);
-        }
-        if (info.file.status === 'done') {
-          message.success(`${info.file.name} file uploaded successfully`);
-        } else if (info.file.status === 'error') {
-          message.error(`${info.file.name} file upload failed.`);
-        }
-      },
-    };
-
 
     return (
 
@@ -156,11 +136,6 @@ class LoginPage extends Component {
             </Link>
           </div>
         </Login>
-        <Upload {...uploadProps}>
-          <Button>
-            <Icon type="upload"/> Click to Upload
-          </Button>
-        </Upload>
       </div>
 
     );
