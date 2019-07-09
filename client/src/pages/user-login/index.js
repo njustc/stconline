7f0db52a3b15622d3a5e7eb33b2cda4aaa0bc0d5
mkx22 <3476799672@qq.com>
2019-07-09 12:06:57 +0800

    login添加图片

--- client/src/pages/user-login/index.js
+++ client/src/pages/user-login/index.js
@@ -8,7 +8,6 @@ import router from 'umi/router';
 import Button from "antd/es/button";
 import styles from './style.less';
 import {readCookie, getRole} from "../../utils/cookieUtils";
-
 const {Tab, UserName, Password, Mobile, Captcha, Submit} = Login;
 
 @connect(({userInfo, loading}) => ({
@@ -87,6 +86,12 @@ class LoginPage extends Component {
             this.loginForm = form;
           }}
         >
+
+          <img src="http://ise.nju.edu.cn/author/admin/avatar_hu65273352f63401a5ced919c03e133798_24423_250x250_fill_q90_lanczos_center.jpg"/>
+          {/*<div>*/}
+          {/*  <img src={require('src/assets/timg.jpeg')} />*/}
+          {/*</div>*/}
+          <br/>
           <UserName
             name="username"
             placeholder={`${formatMessage({id: 'user-login.login.userName'})}`}
@@ -108,14 +113,14 @@ class LoginPage extends Component {
             ]}
             onPressEnter={() => this.loginForm.validateFields(this.handleSubmit)}
           />
-          <div>
-            <Checkbox checked={autoLogin} onChange={this.changeAutoLogin}>
-              <FormattedMessage id="user-login.login.remember-me"/>
-            </Checkbox>
-            <a style={{float: 'right'}} href="">
-              <FormattedMessage id="user-login.login.forgot-password"/>
-            </a>
-          </div>
+          {/*<div>*/}
+          {/*  <Checkbox checked={autoLogin} onChange={this.changeAutoLogin}>*/}
+          {/*    <FormattedMessage id="user-login.login.remember-me"/>*/}
+          {/*  </Checkbox>*/}
+          {/*  <a style={{float: 'right'}} href="">*/}
+          {/*    <FormattedMessage id="user-login.login.forgot-password"/>*/}
+          {/*  </a>*/}
+          {/*</div>*/}
           <Submit loading={submitting}>
             <FormattedMessage id="user-login.login.login"/>
           </Submit>
@@ -124,16 +129,16 @@ class LoginPage extends Component {
             onClick={this.handleLogout}
           >登出
           </Button>
-          <div className={styles.other}>
-            <FormattedMessage id="user-login.login.sign-in-with"/>
-            <Icon type="alipay-circle" className={styles.icon} theme="outlined"/>
-            <Icon type="taobao-circle" className={styles.icon} theme="outlined"/>
-            <Icon type="weibo-circle" className={styles.icon} theme="outlined"/>
-            <Link className={styles.register} to="/user/register">
-              <FormattedMessage id="user-login.login.signup"/>
-            </Link>
-          </div>
-          <a href="http://localhost:8080/api/project/files?pid=p20190610010104&filename=testfile">下载文件</a>
+          {/*<div className={styles.other}>*/}
+          {/*  <FormattedMessage id="user-login.login.sign-in-with"/>*/}
+          {/*  <Icon type="alipay-circle" className={styles.icon} theme="outlined"/>*/}
+          {/*  <Icon type="taobao-circle" className={styles.icon} theme="outlined"/>*/}
+          {/*  <Icon type="weibo-circle" className={styles.icon} theme="outlined"/>*/}
+          {/*  <Link className={styles.register} to="/user/register">*/}
+          {/*    <FormattedMessage id="user-login.login.signup"/>*/}
+          {/*  </Link>*/}
+          {/*</div>*/}
+          {/*<a href="http://localhost:8080/api/project/files?pid=p20190610010104&filename=testfile">下载文件</a>*/}
         </Login>
       </div>
 
